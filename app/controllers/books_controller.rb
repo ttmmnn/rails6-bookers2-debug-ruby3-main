5256aa0067212468f6c57e5ad58120f766c0644f
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-04-05 08:36:13 +0000

    [complete]応用課題7a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
   def index
     to = Time.current.at_end_of_day
     from = (to - 6.day).at_beginning_of_day
-    @books = Book.all.sort_by {|x| x.favorites.where(created_at: from...to).size}.reverse
+    @books = Book.includes(:favorites).sort_by {|x| x.favorites.where(created_at: from...to).size}.reverse
     @book = Book.new
   end
 
